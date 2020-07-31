ef74e1cfb095bc6c632ffeb6afde8b89fd49b3da
Nicole Erler <n.erler@erasmusmc.nl>
2020-07-31 08:17:07 +0200

    include sim_outcome_surv_jm()

--- R/sim_outcome_jm.R
+++ R/sim_outcome_jm.R
@@ -1,99 +1,202 @@
-# sim_outcome_jm <- function(dat, formula, betas, sigma, phi, mean.Cens, D, ...) {
-#   # betas should be a list with elements 'surv' and 'long'
-#   # formula should be a list with elements 'surv' and 'long'
-#   # D: random effects design matrix should be a list
-#   fmla_long <- JointAI:::split_formula_list(JointAI:::check_formula_list(formula$long))
-#   groups <- JointAI:::get_groups(JointAI:::extract_id(fmla_long$random), dat)
-#   group_lvls <- colSums(!JointAI:::identify_level_relations(groups))
-#
-#
-#   survrow <- match(unique(groups[[names(group_lvls)[which(group_lvls == 2)]]]),
-#                    groups[[names(group_lvls)[which(group_lvls == 2)]]])
-#
-#   datsurv <- dat[survrow, ]
-#
-#   b <- sapply(D, function(d) {
-#     n <- sapply(groups[names(d)], function(x) {length(unique(x))})
-#     bs <- mapply(function(nn, dd) {
-#       MASS::mvrnorm(nn, rep(0, ncol(dd)), dd)
-#     }, dd = d, nn = n, SIMPLIFY = FALSE)
-#
-#     sapply(names(bs), function(k) bs[[k]][groups[[k]], ], simplify = FALSE)
-#   }, simplify = FALSE)
-#
-#   Xlong <- sapply(JointAI:::remove_LHS(fmla_long$fixed),
-#                   model.matrix, data = dat, simplify = FALSE)
-#
-#   Zlong <- sapply(fmla_long$random, function(fmla) {
-#     sapply(JointAI:::remove_grouping(fmla),
-#            model.matrix, data = dat, simplify = FALSE)}, simplify = FALSE)
-#
-#   datlong <- cbind(dat, mapply(function(X, beta, Z, b) {
-#     as.vector(X %*% beta[colnames(X)]) +
-#       rowSums(mapply(function(Z, b) rowSums(Z * b), Z = Z, b = b)) +
-#       rnorm(nrow(X), 0, sd = sigma)
-#   }, X = Xlong, beta = betas$long, Z = Zlong, b = b))
-#
-#
-#   invS <- function(t, u, i) {
-#     h <- function(s) {
-#
-#       dattemp <- dat[rep(survrow[i], length(s)), , drop = FALSE]
-#       dattemp$time <- s
-#
-#       Xl <- sapply(JointAI:::remove_LHS(fmla_long$fixed),
-#                    model.matrix, data = dattemp, simplify = FALSE)
-#
-#       Zl <- sapply(fmla_long$random, function(fmla) {
-#         sapply(JointAI:::remove_grouping(fmla),
-#                model.matrix, data = dattemp, simplify = FALSE)}, simplify = FALSE)
-#
-#       dattemp <- cbind(dattemp, mapply(function(X, beta, Z, b) {
-#         as.vector(X %*% beta[colnames(X)]) +
-#           rowSums(mapply(function(Z, b) rowSums(Z * b[rep(survrow[i], length(s)), , drop = FALSE]),
-#                          Z = Z, b = b))
-#       }, X = Xl, beta = betas$long, Z = Zl, b = b, SIMPLIFY = FALSE))
-#
-#
-#       Xsurv <- model.matrix(formula$surv, dattemp)
-#       if (any(is.na(match(colnames(Xsurv), names(betas$surv)))))
-#         stop("The names of the coefficients do not match the names of the survival design matrix.")
-#
-#       exp(log(phi) + (phi - 1) * log(s) + as.vector(Xsurv %*% betas$surv[colnames(Xsurv)]))
-#     }
-#     integrate(h, lower = 0, upper = t)$value + log(u)
-#   }
-#
-#
-#   u <- stats::runif(length(survrow))
-#   trueTimes <- numeric(length(survrow))
-#   for (i in 1:length(survrow)) {
-#     Up <- 500
-#     tries <- 5
-#     Root <- try(uniroot(invS, interval = c(1e-05, Up), u = u[i], i = i)$root, TRUE)
-#     while (inherits(Root, "try-error") && tries > 0) {
-#       tries <- tries - 1
-#       Up <- Up + 500
-#       Root <- try(uniroot(invS, interval = c(1e-05, Up), u = u[i], i = i)$root, TRUE)
-#     }
-#     trueTimes[i] <- if (!inherits(Root, "try-error")) Root else NA
-#   }
-#
-#
-#   # simulate censoring times from an exponential distribution,
-#   # and calculate the observed event times, i.e., min(true event times, censoring times)
-#   Ctimes <- stats::runif(length(trueTimes), 0, 2 * mean.Cens)
-#   datsurv$etime <- pmin(trueTimes, Ctimes)
-#   datsurv$event <- as.numeric(trueTimes <= Ctimes) # event indicator
-#
-#   tdat <- subset(merge(subset(datsurv, select = c(id, etime)),
-#                        subset(datlong, select = c('id', 'time' , names(D)))), time <= etime)
-#
-#   ldat <- merge(tdat,
-#                 subset(datsurv, select = c(id, etime), datsurv$id %in% tdat$id),
-#                 by.x = c('id', 'time'), by.y = c('id', 'etime'), all = TRUE)
-#
-#   list(merge(subset(ldat),
-#              subset(datsurv, select = c('id', setdiff(names(datsurv), names(ldat))))
-#   ), trueTimes = trueTimes, Ctimes = Ctimes)
-# }
+#' Simulate a survival and longitudinal outcome for a joint model
+#' @param data a `data.frame` containing covariate data
+#' @param formula a list of model formulas for the survival and longitudinal
+#'                outcomes
+#' @param reg_coefs a list of regression coefficients with elements "surv" and
+#'                  "long". Each of the list elements is a (named) vector.
+#'                  If named, the names will be matched with the names of the
+#'                  design matrix that is created from `formula` and `data`.
+#' @param ranef_vcov a random effects variance-covariance matrix or a named
+#'                   list of such matrices when there are more than two levels
+#'                   (and names being equal to the grouping variables)
+#' @param phi parameter of the Wishart distribution
+#' @param mean_cens mean censoring time
+#' @param .tries integer; how often is the upper limit increased when looking
+#'               for the root
+#' @param seed the seed value
+#' @param ... arguments passed to other functions
+#'
+#' @export
+#'
+
+sim_outcome_surv_jm <- function(data, formula, reg_coefs, resid_sd,
+                                beta_Bh0 = NULL, ranef_vcov, type,
+                                timevar = "time",
+                                basehaz_type = "splines",
+                                phi = 1.2, mean_cens = 30.0,
+                                .tries = 5, .up = 500L, up_step = 500L,
+                                seed = NULL, no_obs_after_event = TRUE,
+                                no_subset = FALSE, progress_bar = FALSE,
+                                ...) {
+
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+
+  # model formulas
+  fmla_long <- split_formula_list(check_formula_list(formula$long))
+  fmla_fixed <- lapply(fmla_long$fixed, function(x) {
+    as.formula(deparse(x[-2], width.cutoff = 500))
+  })
+  fmla_random <- remove_grouping(fmla_long$random)
+
+
+  # groups
+  idvars <- extract_id(fmla_long$random)
+  groups <- get_groups(idvars, data)
+  group_lvls <- colSums(!identify_level_relations(groups))
+
+  if (any(group_lvls[duplicated(group_lvls)] %in% c(1, 2))) {
+    errormsg("Is there a mistake in the grouping variables? There should only
+             be one grouping variable for level-1 and one for level-2.")
+  }
+
+
+  # survival rows and subset
+  surv_row <- match(unique(groups[[names(group_lvls)[which(group_lvls == 2)]]]),
+                    groups[[names(group_lvls)[which(group_lvls == 2)]]])
+
+  surv_data <- data[surv_row, ]
+
+  fmla_surv <- split_formula_tvar(formula$surv, data = data, groups = groups)
+
+  desgn_mat_surv_tconst <- model_matrix(fmla_surv$tconst, surv_data)
+
+  if (basehaz_type == "spline") {
+    desgn_mat_surv_tconst <- desgn_mat_surv_tconst[, -1L]
+    kn <- get_knots_h0(nkn = 2,
+                       Time = 0:(mean_cens * 4),
+                       gkx = gauss_kronrod()$gkx)
+    kn[length(kn)] <- 100 * kn[length(kn)]
+
+  }
+
+  lp_surv_tconst <- desgn_mat_surv_tconst %*%
+    select_coefs(reg_coefs$surv, desgn_mat_surv_tconst)
+
+
+
+  long_out <- sim_outcome_glmm(data = data,
+                               formula = formula$long,
+                               reg_coefs = reg_coefs$long,
+                               resid_sd = resid_sd,
+                               ranef_vcov = ranef_vcov,
+                               type = type, return_ranefs = TRUE)
+
+  data = long_out$data
+  ranefs = long_out$ranefs
+
+  inv_survival <- function(t, u, i) {
+    h <- function(times) {
+
+      rows <- rep(surv_row[i], length(times))
+      temp_data <- data[rows, , drop = FALSE]
+      temp_data[[timevar]] <- times
+
+      temp_data <- sim_outcome_glmm2(data = temp_data,
+                                     fmla_fixed = fmla_fixed,
+                                     fmla_random = fmla_random,
+                                     reg_coefs = reg_coefs$long,
+                                     resid_sd = resid_sd,
+                                     type = type,
+                                     ranefs = lapply(ranefs, function(v) {
+                                       lapply(v, function(lvl) {
+                                         lvl[rows, , drop = FALSE]
+                                       })
+                                     })
+      )
+
+      desgn_mat_surv_tvar <- model_matrix(fmla_surv$tvar, temp_data)
+      lp_surv_tvar <- desgn_mat_surv_tvar %*%
+        select_coefs(reg_coefs$surv, desgn_mat_surv_tvar)
+
+
+      if (basehaz_type == "weibull") {
+        exp(log(phi) + (phi - 1L) * log(times) +
+              as.vector(lp_surv_tconst) + as.vector(lp_surv_tvar))
+      } else if (basehaz_type == "spline") {
+        desgn_mat_basehaz <- splines::splineDesign(kn, times, ord = 4L,
+                                                   outer.ok = TRUE)
+
+        # if (tries > 1){
+        #   plot(times, desgn_mat_basehaz %*% beta_Bh0)
+        # }
+
+        exp(c(desgn_mat_basehaz %*% beta_Bh0) +
+              as.vector(lp_surv_tconst)[i] + as.vector(lp_surv_tvar))
+      }
+    }
+    integrate(h, lower = 0L, upper = t, subdivisions = 500L,
+              rel.tol = .Machine$double.eps^0.2)$value + log(u)
+  }
+
+  u <- runif(length(surv_row))
+  true_times <- nr_tries <- numeric(length(surv_row))
+
+  if (progress_bar) {
+    pb <- txtProgressBar(char = "*", width = 60, style = 3)
+  }
+  for (i in seq_along(surv_row)) {
+    if (progress_bar) {
+      setTxtProgressBar(pb, i/length(surv_row))
+    }
+    up <- .up
+    tries <- 1
+    root <- try(uniroot(inv_survival,
+                        interval = c(1.0e-05, up), u = u[i], i = i)$root, TRUE)
+
+    while (inherits(root, "try-error") && tries < .tries) {
+      tries <- tries + 1L
+      up <- up + up_step
+      root <- try(uniroot(inv_survival,
+                          interval = c(1.0e-05, up),
+                          u = u[i], i = i)$root, TRUE)
+    }
+    true_times[i] <- if (!inherits(root, "try-error")) {
+      root
+    } else if (tries == .tries) {
+      Inf
+    } else {
+      NA
+    }
+    nr_tries[i] <- tries
+  }
+  if (progress_bar) {
+    close(pb)
+  }
+  # simulate censoring times from an exponential distribution, and calculate the
+  # observed event times, i.e., min(true event times, censoring times)
+  cens_times <- runif(length(true_times), 0L, 2L * mean_cens)
+
+  surv_data[, all.vars(formula$surv[[2L]])] <-
+    cbind(pmin(true_times, cens_times),
+          as.numeric(true_times <= cens_times))
+  # surv_data$true_times <- true_times
+
+  # merge the survival outcome with the longitudinal data
+  if (no_subset) {
+    jm_data <- merge(subset(surv_data,
+                            select = c(idvars, all.vars(formula$surv[[2L]]))),
+                     data)
+  } else {
+    jm_data <- merge(subset(surv_data, !is.na(true_times),
+                            select = c(idvars, all.vars(formula$surv[[2L]]))),
+                     data)
+  }
+
+
+  if (no_obs_after_event) {
+    jm_data <- jm_data[jm_data[[timevar]] <=
+                         jm_data[[all.vars(formula$surv[[2L]])[1]]],,
+                       drop = FALSE]
+  }
+  attr(jm_data, "true_times") <- true_times
+  attr(jm_data, "cens_times") <- cens_times
+  attr(jm_data, "nr_tries") <- nr_tries
+  attr(jm_data, "no_obs_after_event") <- no_obs_after_event
+  attr(jm_data, "timevar") <- timevar
+  attr(jm_data, "size_orig") <- ivapply(groups, function(x) length(unique(x)))
+  # attr(jm_data, "lpconst") <- lp_surv_tconst
+
+  jm_data
+}
