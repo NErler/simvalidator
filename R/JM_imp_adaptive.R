35b0a21132430ba43161923817727b1606596d1a
Nicole Erler <n.erler@erasmusmc.nl>
2020-08-07 12:33:26 +0200

    new function for adaptive running of JM_imp()

---
+++ R/JM_imp_adaptive.R
@@ -0,0 +1,102 @@
+#' Run a joint model with adaptively increasing the number of iterations
+#' @inheritParams JointAI::JM_imp
+#' @inheritParams as_inits
+#' @inheritParams run_gr_check
+#' @param inits_iter number of iteration used for the model that generates the
+#' initial values
+#' @export
+JM_imp_adaptive <- function(formula, data, df_basehaz = 6,
+                            n.chains = 3, n.adapt = 100, n.iter = 0, thin = 1,
+                            monitor_params = c(analysis_main = TRUE),
+                            auxvars = NULL,
+                            timevar = NULL, refcats = NULL,
+                            models = NULL, no_model = NULL,
+                            assoc_type = NULL, trunc = NULL,
+                            shrinkage = FALSE, ppc = TRUE, seed = NULL,
+                            scale_vars = NULL, hyperpars = NULL,
+                            modelname = NULL, modeldir = NULL,
+                            keep_model = FALSE, overwrite = NULL,
+                            quiet = TRUE, progress.bar = "text",
+                            warn = TRUE, mess = TRUE,
+                            keep_scaled_mcmc = FALSE, inits_iter = 200,
+                            what = c("RinvD", "invD", "tau", "b"),
+                            extra_iter = NULL,
+                            minsize = 500L, step = 200L, subset = NULL,
+                            cutoff = 1.2, prop = 0.8,
+                            gr_max = 1.5, max_try = 5L,
+                            ...) {
+
+  args <- as.list(match.call()[-1L])
+
+  inits <- get_inits_JM(formula = formula, data = data,
+                        inits_iter = inits_iter, n_chains = n.chains)
+  args$inits <- inits
+
+  fitted_model <- do.call(JointAI::JM_imp, args)
+
+  check_list <- run_gr_check(fitted_model = fitted_model,
+                             extra_iter = extra_iter, minsize = minsize,
+                             step = step, subset = subset, cutoff = cutoff,
+                             prop = prop,
+                             gr_max = gr_max, max_try = max_try)
+
+  chains <- seq_along(check_list$fitted_model$MCMC)
+  if (!is.null(check_list$exclude_chains)) {
+    chains <- chains[-check_list$exclude_chains]
+  }
+
+  check_list$fitted_model$MCMC <- window(check_list$fitted_model$MCMC[chains],
+                                         start = check_list$strt)
+  check_list$fitted_model
+}
+
+
+
+
+
+
+
+
+get_inits_JM <- function(formula, data, inits_iter, n_chains) {
+
+  prep <- JointAI::lme_imp(formula[-1], data = data, n.iter = inits_iter,
+                           n.chains = n_chains)
+
+  as_inits(prep)
+}
+
+
+#' Return the current state of a 'JointAI' model as list to be used as
+#' initial values
+#' @param object an object of class 'JointAI'
+#' @param what vector of node types
+#'
+#'
+as_inits <- function(object,
+                     what = c("RinvD", "invD", "tau", "b")) {
+
+  jags_model_type <- if (inherits(object$model, "list")) {
+    "list"
+  } else {
+    "jags"
+  }
+
+  nodes <- unlist(
+    lapply(paste0("^", what, "_"),
+           grep,
+           x = switch(jags_model_type,
+                      "list" = names(object$model[[1]]$state()[[1]]),
+                      "jags" = names(object$model$state()[[1]])
+           ),
+           value = TRUE)
+  )
+
+  switch(jags_model_type,
+         "list" = lapply(object$model, function(mod) {
+           mod$state()[[1]][nodes]
+         }),
+         "jags" = lapply(object$model$state(), function(chain) {
+           chain[nodes]
+         })
+  )
+}
