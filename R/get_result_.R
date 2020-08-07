9a969daeee30f99aaf450fa32e95e32b121316e8
Nicole Erler <n.erler@erasmusmc.nl>
2020-08-07 12:29:20 +0200

    update get_results_JointAI() to handle results from models with mutliple sub-models

--- R/get_result_.R
+++ R/get_result_.R
@@ -38,22 +38,50 @@ get_result_default <- function(fitted_model, type = NA, seed = NA, ...) {
 get_result_JointAI <- function(fitted_model, seed = NA, outcome = 1L,
                                subset = NULL, ...) {
 
-  res <- data.frame(
-    seed = seed,
-    time = as.numeric(fitted_model$comp_info$duration, units = "hours"),
-    type = 'JointAI',
-    n_iter = nrow(fitted_model$MCMC[[1]]),
-    n_chain = length(fitted_model$MCMC),
-    variable = names(coef(fitted_model, subset = subset)[[outcome]]),
-    Mean = coef(fitted_model, subset = subset)[[outcome]],
-    confint(fitted_model, subset = subset)[[outcome]],
-    "GR-crit" = JointAI::GR_crit(fitted_model, subset = subset,
-                                 autoburnin = FALSE,
-                                 multivariate = FALSE)[[1]][, 2],
-    "MCE/SD" = JointAI::MC_error(fitted_model,
-                                 subset = subset)$data_scale[, "MCSE/SD"],
-    check.names = FALSE
+  gr_crit <- JointAI::GR_crit(fitted_model, subset = subset,
+                              autoburnin = FALSE,
+                              multivariate = FALSE)[[1]][, 2]
+  mce <- JointAI::MC_error(fitted_model,
+                           subset = subset)$data_scale[, "MCSE/SD"]
+
+  pars <- JointAI::parameters(fitted_model)
+  pars$varname[is.na(pars$varname)] <- pars$coef[is.na(pars$varname)]
+
+  coefs <- coef(fitted_model)
+  cis <- confint(fitted_model)
+
+  res <- Reduce(merge,
+                list(
+                  data.frame(
+                    seed = seed,
+                    time = sum(as.numeric(fitted_model$comp_info$duration,
+                                          units = "hours")),
+                    type = 'JointAI',
+                    n_iter = nrow(fitted_model$MCMC[[1]]),
+                    n_chain = length(fitted_model$MCMC)
+                  ),
+                  do.call(rbind,
+                          lapply(names(coefs), function(out) {
+                            data.frame(outcome = JointAI::clean_survname(out),
+                                       variable = names(coefs[[out]]),
+                                       Mean = coefs[[out]]
+                            )
+                          })
+                  ),
+                  do.call(rbind,
+                          lapply(names(cis), function(out) {
+                            data.frame(outcome = JointAI::clean_survname(out),
+                                       variable = rownames(cis[[out]]),
+                                       cis[[out]],
+                                       check.names = FALSE
+                            )
+                          })
+                  ),
+                  match_crit(pars, gr_crit, name = "GR-crit"),
+                  match_crit(pars, mce, name = "MCE/SD")
+                )
   )
+
 
   attr(res, "pkg_version") <- fitted_model$comp_info$JointAI_version
   attr(res, "future") <- fitted_model$comp_info$future
@@ -61,4 +89,24 @@ get_result_JointAI <- function(fitted_model, seed = NA, outcome = 1L,
   attr(res, "platform") <- fitted_model$comp_info$sessionInfo$platform
 
   res
+}
+
+
+
+match_crit <- function(parameters, crit, name) {
+  rows <- if (length(unique(parameters$outcome)) == 1) {
+    match(parameters$varname, names(crit))
+  } else {
+    match1 <- match(paste0(parameters$outcome, ": ",
+                           parameters$varname), names(crit))
+    match2 <- match(parameters$coef, names(crit))
+    match1[is.na(match1)] <- match2[is.na(match1)]
+    match1
+  }
+
+  df <- data.frame(outcome = parameters$outcome[rows],
+                   variable = parameters$varname[rows])
+  df[[name]] <- crit
+
+  df
 }
