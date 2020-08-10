8ba3a72d59c48a7d50ca17b8a22ecf0987aa7d54
N.S. Erler <n.erler@erasmusmc.nl>
2020-08-10 09:20:56 +0200

    update report template

--- inst/templates/report_template.R
+++ inst/templates/report_template.R
@@ -38,11 +38,14 @@ res_df <- get_res_df(object)
 
 
 #' ## Bias and CI width {.tabset}
+n_rows <- ceiling(nrow(unique(subset(res_df, !is.na(bias),
+                                     select = c("variable", "outcome"))))/4)
 #' ### absolute bias
+#+ fig.width = 8, fig.height = n_rows * 2
 ggplot(subset(res_df, !is.na(bias)),
        aes(x = type, y = bias)) +
   geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) +
-  facet_wrap("variable ~ outcome", scales = 'free') +
+  facet_wrap("outcome ~ variable", scales = 'free', ncol = 4) +
   geom_hline(yintercept = 0, linetype = 2, size = 1, color = 'darkgreen') +
   geom_jitter(height = 0, width = 0.2, alpha = 0.3) +
   xlab('') +
@@ -50,10 +53,11 @@ ggplot(subset(res_df, !is.na(bias)),
 
 
 #' ### relative bias
+#+ fig.width = 8, fig.height = n_rows * 2
 ggplot(subset(res_df, !is.na(relbias)),
        aes(x = type, y = relbias)) +
   geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) +
-  facet_wrap("variable ~ outcome", scales = 'free') +
+  facet_wrap("outcome ~ variable", scales = 'free', ncol = 4) +
   geom_hline(yintercept = 0, linetype = 2, size = 1, color = 'darkgreen') +
   geom_jitter(height = 0, width = 0.2, alpha = 0.3) +
   xlab('') +
@@ -61,10 +65,11 @@ ggplot(subset(res_df, !is.na(relbias)),
 
 
 #' ### parameter estimate
+#+ fig.width = 8, fig.height = n_rows * 2
 ggplot(subset(res_df, !is.na(bias)),
        aes(x = type, y = Mean)) +
   geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) +
-  facet_wrap("variable ~ outcome", scales = 'free') +
+  facet_wrap("outcome ~ variable", scales = 'free', ncol = 4) +
   geom_hline(aes(yintercept = true_param),
              linetype = 2, size = 1, color = 'darkgreen') +
   geom_jitter(height = 0, width = 0.2, alpha = 0.3) +
@@ -73,10 +78,11 @@ ggplot(subset(res_df, !is.na(bias)),
 
 
 #' ### CI width
+#+ fig.width = 8, fig.height = 8
 ggplot(subset(res_df, !is.na(bias)),
        aes(x = type, y = CIwidth)) +
   geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) +
-  facet_wrap("variable", scales = 'free') +
+  facet_wrap("outcome ~ variable", scales = 'free') +
   geom_jitter(height = 0, width = 0.2, alpha = 0.3) +
   xlab('') +
   ylab('width of 95% CI')
@@ -85,7 +91,7 @@ ggplot(subset(res_df, !is.na(bias)),
 #' ## Coverage & MSE
 #' ### Coverage and MSE
 subset(res_df, !is.na(covrg)) %>%
-  plyr::ddply(c('type', 'variable'), plyr::summarize,
+  plyr::ddply(c('type', "outcome", 'variable'), plyr::summarize,
         covrg = mean(covrg),
         MSE = mean(bias^2)) %>%
   kable(digits = 3) %>%
@@ -94,6 +100,7 @@ subset(res_df, !is.na(covrg)) %>%
 
 #' ## MCMC criteria
 #' ### Gelman-Rubin criterion
+#+ fig.width = 8, fig.height = 8
 ggplot(subset(res_df, !is.na(`GR-crit`)),
        aes(x = type, y = `GR-crit`)) +
   geom_violin(draw_quantiles = 0.5) +
@@ -104,10 +111,11 @@ ggplot(subset(res_df, !is.na(`GR-crit`)),
   scale_color_gradient2(high = scales::muted("red"), mid = "yellow",
                         low = "darkgreen", midpoint = 1.2) +
   ylab('Gelman-Rubin criterion') +
-  facet_wrap("variable ~ outcome")
+  facet_wrap("outcome ~ variable")
 
 
 #' ### Monte-Carlo error
+#+ fig.width = 8, fig.height = 8
 ggplot(subset(res_df, !is.na(`MCE/SD`)), aes(x = type, y = `MCE/SD`)) +
   geom_violin(draw_quantiles = 0.5) +
   geom_jitter(height = 0, alpha = 0.3, width = 0.3,
@@ -116,4 +124,4 @@ ggplot(subset(res_df, !is.na(`MCE/SD`)), aes(x = type, y = `MCE/SD`)) +
   scale_color_gradient2(high = scales::muted("red"), mid = "yellow",
                         low = "darkgreen", midpoint = 0.06) +
   ylab('Monte-Carlo Error / Posterior SD') +
-  facet_wrap("variable")
+  facet_wrap("outcome ~ variable")
