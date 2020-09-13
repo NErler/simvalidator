e121117aa05ca69280af959f0ca40b2bae4694c3
Nicole Erler <n.erler@erasmusmc.nl>
2020-09-13 15:34:29 +0200

    update create_report to handle folders of simulations batches as input

--- R/create_report.R
+++ R/create_report.R
@@ -11,8 +11,16 @@ create_report <- function(object, title = NULL,
 
   if (inherits(object, "character")) {
     object_path <- object
-    object_name <- load(object_path)
-    object <- get(object_name)
+    if (grepl(".RData$", object_path)) {
+      object_name <- load(object_path)
+      object <- get(object_name)
+    } else if (length(dir(object_path)) > 0) {
+      object_list <- lapply(dir(object_path, full.names = TRUE), function(x){
+        obj_name <- load(x);
+        get(obj_name)
+      })
+      object <- collapse_sim_batches(object_list)
+    }
   }
 
   rmarkdown::render(system.file("templates", "report_template.R",
@@ -22,4 +30,29 @@ create_report <- function(object, title = NULL,
                     output_dir = output_dir, ...)
 
   system2("open", file.path(output_dir, "simulation_report.html"))
+}
+
+
+
+
+collapse_sim_batches <- function(object) {
+  sim_res <- unlist(lapply(object, "[[", "sim_res"), recursive = FALSE)
+  time <- do.call(sum, lapply(object, "[[", "time"))
+  file_name <- cvapply(object, "[[", "file_name")
+
+  l <- nlapply(setdiff(names(object[[1]]), c("sim_res", "time", "file_name")),
+               function(k) {
+                 x <- unique(lapply(object, "[[", k))
+                 if (length(x) > 1) {
+                   if (length(unique(lapply(x, deparse))) > 1) {
+                     errormsg("There are different versions of %s.", dQuote(k))
+                   }
+                 }
+                 x[[1]]
+               })
+  c(
+    list(sim_res = sim_res,
+         time = time,
+         file_name = file_name),
+    l)
 }
