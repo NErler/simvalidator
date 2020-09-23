75181c80b9cfc6da4d3e176c9cf7c2d38ebdf2c7
N.S. Erler <n.erler@erasmusmc.nl>
2020-09-23 11:13:39 +0200

    change run_sim_batch to first simulate data to be able to run multiple scenarios in parallel

--- R/run_sim.R
+++ R/run_sim.R
@@ -49,7 +49,7 @@ run_sim <- function(sim_pars, covar_def, outcome_pars, models,
   folder <- paste0("sim_", outcome_pars$response_type,
                    if (!is.null(sim_name)) paste0("_", sim_name),
                    "_", sim_pars$global_seed,
-                   "-", sim_pars$nr_sims)
+                   "-", sim_pars$nr_sims, "_", format(Sys.Date(), "%Y-%m-%d"))
 
   if (!dir.exists(folder)) {
     dir.create(file.path(path, folder))
@@ -100,9 +100,8 @@ run_sim_batch <- function(seeds,
 
   t0 <- Sys.time()
 
-  sim_res <- foreach::`%dopar%`(
-    foreach::foreach(seed = seeds,
-                     .packages = packages),
+  datlist <- foreach::`%dopar%`(
+    foreach::foreach(seed = seeds, .packages = packages),
     {
       # simulate complete data
       data_orig <- sim_data(covar_def, outcome_pars, seed = seed)
@@ -120,34 +119,46 @@ run_sim_batch <- function(seeds,
         data_lvls = data_lvls
       )
 
-      scen_res <- foreach::`%do%`(
-        foreach::foreach(scen = names(mis_scenarios),
-                         .packages = packages,
-                         .final = function(x)
-                           setNames(x, names(mis_scenarios))),
-        {
-          data <- create_missingness(data_orig, mis_scenarios[[scen]],
-                                     idvars = setdiff(names(groups), "lvlone"),
-                                     groups = groups, varlvls = data_lvls)
-          res <- if (!skip_fit) {
-            fit_models(models, formula = outcome_pars$formula,
-                       data = data, seed = seed, scen = scen)
-          }
-          data_info <- get_miss_data_info(
-            data,
-            seed,
-            scen = scen,
-            idvars = names(groups),
-            data_lvls = data_lvls
-          )
-          list(res = res,
-               data_info = data_info)
-        }
+      data_list <- lapply(names(mis_scenarios), function(scen) {
+        list(
+          data = create_missingness(data_orig, mis_scenarios[[scen]],
+                                    idvars = setdiff(names(groups), "lvlone"),
+                                    groups = groups, varlvls = data_lvls),
+          scen = scen,
+          seed = seed,
+          idvars = names(groups),
+          data_lvls = data_lvls
+        )
+      })
+      list(data_list = data_list,
+           compl_data_info = compl_data_info)
+    })
+
+  compl_data_info <- lapply(datlist, "[[", "compl_data_info")
+  data_list <- unlist(lapply(datlist, "[[", "data_list"), recursive = FALSE)
+
+
+  batch_res <- foreach::`%dopar%`(
+    foreach::foreach(d_list = data_list,
+                     .packages = packages),
+    {
+      data_info <- get_miss_data_info(
+        data = d_list$data,
+        seed = d_list$seed,
+        scen = d_list$scen,
+        idvars = d_list$idvars,
+        data_lvls = d_list$data_lvls
       )
 
-      list(compl_data_info = compl_data_info,
-           scen_res = scen_res)
+      res <- if (!skip_fit) {
+        fit_models(models, formula = outcome_pars$formula,
+                   data = d_list$data, seed = d_list$seed, scen = d_list$scen)
+      }
+
+      list(res = res,
+           data_info = data_info)
     })
+
 
   t1 <- Sys.time()
 
@@ -159,7 +170,8 @@ run_sim_batch <- function(seeds,
   file_name <- paste0(file_name, "_",
                       format(Sys.time(), "%Y-%m-%d_%H-%M"), ".RData")
   out <- structure(
-    list(sim_res = sim_res,
+    list(sim_res = batch_res,
+         compl_data_info = compl_data_info,
          sim_pars = sim_pars,
          covar_def = covar_def,
          mis_scenarios = mis_scenarios,
@@ -183,6 +195,104 @@ run_sim_batch <- function(seeds,
 
   out
 }
+
+
+# run_sim_batch <- function(seeds,
+#                           sim_pars, covar_def, outcome_pars, models,
+#                           mis_scenarios = NULL, skip_fit = FALSE,
+#                           packages, file_name, folder, path) {
+#
+#   oplan <- future::plan(future::sequential)
+#   future::plan(oplan)
+#
+#   t0 <- Sys.time()
+#
+#   sim_res <- foreach::`%dopar%`(
+#     foreach::foreach(seed = seeds,
+#                      .packages = packages),
+#     {
+#       # simulate complete data
+#       data_orig <- sim_data(covar_def, outcome_pars, seed = seed)
+#
+#       # determine grouping structure and levels of each variable
+#       groups <- get_groups(setdiff(names(outcome_pars$covar_pars$group_lvls),
+#                                    "lvlone"),
+#                            data_orig)
+#       data_lvls <- cvapply(data_orig, check_varlevel, groups = groups)
+#
+#       compl_data_info <- get_compl_data_info(
+#         data_orig,
+#         seed,
+#         idvars = names(groups),
+#         data_lvls = data_lvls
+#       )
+#
+#       scen_res <- foreach::`%do%`(
+#         foreach::foreach(scen = names(mis_scenarios),
+#                          .packages = packages,
+#                          .final = function(x)
+#                            setNames(x, names(mis_scenarios))),
+#         {
+#           data <- create_missingness(data_orig, mis_scenarios[[scen]],
+#                                      idvars = setdiff(names(groups), "lvlone"),
+#                                      groups = groups, varlvls = data_lvls)
+#
+#           data_info <- get_miss_data_info(
+#             data,
+#             seed,
+#             scen = scen,
+#             idvars = names(groups),
+#             data_lvls = data_lvls
+#           )
+#
+#           res <- if (!skip_fit) {
+#             fit_models(models, formula = outcome_pars$formula,
+#                        data = data, seed = seed, scen = scen)
+#           }
+#
+#           list(res = res,
+#                data_info = data_info)
+#         }
+#       )
+#
+#       list(compl_data_info = compl_data_info,
+#            scen_res = scen_res)
+#     })
+#
+#   t1 <- Sys.time()
+#
+#   cpu <- if ("benchmarkme" %in% installed.packages()[, "Package"]) {
+#     benchmarkme::get_cpu()$model_name
+#   }
+#
+#
+#   file_name <- paste0(file_name, "_",
+#                       format(Sys.time(), "%Y-%m-%d_%H-%M"), ".RData")
+#   out <- structure(
+#     list(sim_res = sim_res,
+#          sim_pars = sim_pars,
+#          covar_def = covar_def,
+#          mis_scenarios = mis_scenarios,
+#          outcome_pars = outcome_pars,
+#          models = lapply(models, "[[", "call"),
+#          time = t1 - t0,
+#          file_name = file_name,
+#          path = path,
+#          platform = list(os = Sys.info()["sysname"],
+#                          machine = Sys.info()["machine"],
+#                          r_version = R.version.string,
+#                          cpu = cpu,
+#                          future = attr(oplan[[1L]], "call"),
+#                          workers = formals(oplan[[1L]])$workers
+#          )
+#     ), class = "simulation_result")
+#
+#   if (exists("path") && !is.null(path)) {
+#     save(out, file = file.path(path, folder, file_name))
+#   }
+#
+#   out
+# }
 
 
 
