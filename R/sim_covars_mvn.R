3c638cac12d8fb91aada5673b3d7d52759a914b9
Nicole Erler <n.erler@erasmusmc.nl>
2022-02-01 16:03:23 +0100

    edit in sim_covars wrt simulation of time variable

--- R/sim_covars_mvn.R
+++ R/sim_covars_mvn.R
@@ -21,6 +21,29 @@ sim_covars_mvn <- function(N, covar_pars, seed = NULL, ...) {
   idvars <- names(covar_pars$means)
   group_lvls <- covar_pars$group_lvls
 
+  if (length(group_lvls) > 1) {
+    iddat <- lapply(N, seq.int)
+
+    for (k in sort(unique(group_lvls))[-1]) {
+
+      for (lvl in names(group_lvls)[which(group_lvls == k)]) {
+        lvl_prev <- names(group_lvls)[which(group_lvls == (k - 1))]
+
+        if (length(lvl_prev) > 1) {
+          type <- covar_pars$group_rel[lvl, lvl_prev] +
+            covar_pars$group_rel[lvl_prev, lvl]
+          lvl_prev <- names(type)[type == 1]
+        }
+
+        temp <- rep_lvls(iddat[[lvl]], N[[lvl_prev]])
+
+        if (sum(group_lvls == k) > 1) {
+          temp <- sample(temp)
+        }
+        iddat[[lvl]] <- temp[iddat[[lvl_prev]]]
+      }
+    }
+
   df <- nlapply(idvars, function(lvl) {
 
     new_data <- if (length(covar_pars$means[[lvl]]) > 0L) {
@@ -53,7 +76,7 @@ sim_covars_mvn <- function(N, covar_pars, seed = NULL, ...) {
       if (covar_pars$timevar_pars$name %in% names(new_data)) {
         df_list <- lapply(
           split(new_data,
-                new_data[[names(group_lvls)[group_lvls == group_lvls[[lvl]] + 1]]]),
+                iddat[[names(group_lvls)[group_lvls == group_lvls[[lvl]] + 1]]]),
           function(df) {
             if (covar_pars$timevar_pars$distr == "unif") {
               df[[covar_pars$timevar_pars$name]] <-
@@ -82,28 +105,7 @@ sim_covars_mvn <- function(N, covar_pars, seed = NULL, ...) {
   })
 
 
-  if (length(group_lvls) > 1) {
-  iddat <- lapply(N, seq.int)
 
-  for (k in sort(unique(group_lvls))[-1]) {
-
-    for (lvl in names(group_lvls)[which(group_lvls == k)]) {
-      lvl_prev <- names(group_lvls)[which(group_lvls == (k - 1))]
-
-      if (length(lvl_prev) > 1) {
-        type <- covar_pars$group_rel[lvl, lvl_prev] +
-          covar_pars$group_rel[lvl_prev, lvl]
-        lvl_prev <- names(type)[type == 1]
-      }
-
-      temp <- rep_lvls(iddat[[lvl]], N[[lvl_prev]])
-
-      if (sum(group_lvls == k) > 1) {
-        temp <- sample(temp)
-      }
-      iddat[[lvl]] <- temp[iddat[[lvl_prev]]]
-    }
-  }
 
 
   if (!identical(JointAI:::identify_level_relations(iddat),
@@ -116,4 +118,13 @@ sim_covars_mvn <- function(N, covar_pars, seed = NULL, ...) {
   } else {
     Reduce(merge, df)
   }
+}
+
+
+
+
+
+rep_lvls <- function(lvls, n) {
+  c(rep(lvls, each = floor(n/length(lvls))),
+    sample(lvls, size = n %% length(lvls)))
 }
