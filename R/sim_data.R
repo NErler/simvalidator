1edb82002f3e660502a1653ca420a0a2e322c130
Nicole Erler <n.erler@erasmusmc.nl>
2022-05-10 14:41:08 +0200

    update documentation

--- R/sim_data.R
+++ R/sim_data.R
@@ -1,13 +1,14 @@
 
 #' Simulate data
-#' @param covar_def covariate definition
-#' @param outcome_pars a list of parameters
+#' @param covar_def list with covariate definitions
+#' @param outcome_pars a list of parameters defining the outcome (model)
 #' @param data_seed a seed value
 #' @export
 sim_data <- function(covar_def, outcome_pars, data_seed = NULL) {
 
   out_fun_name = paste0("sim_outcome_", outcome_pars$response_type)
 
+  # check if the function to simulate the outcome exists
   if (!inherits(try(get(out_fun_name)), "function")) {
     errormsg("I cannot find a function %s to simulate an outcome of type %s.",
              dQuote(out_fun_name), dQuote(outcome_pars$response_type))
