142a13576c626a9dd7683fa3ef0c4870878beb6e
Nicole Erler <n.erler@erasmusmc.nl>
2022-05-10 14:40:51 +0200

    update documentation sim_data_batch, add mis_scenarios as argument, changes to make it work with the new missing data generation

--- R/sim_data_batch.R
+++ R/sim_data_batch.R
@@ -1,22 +1,32 @@
 
 
+#' Simulate data for a batch of simulations
+#'
+#' @param data_seeds vector of seeds to use for generating the data
+#' @param covar_def covariate definitions
+#' @param outcome_pars list with setting defining the outcome (model)
+#' @param packages optional names of packages needed to be passed to
+#'                 `future` (for parallel processing)
+#' @param mis_scenarios list of missingness scenarios
+#'
+#' @export
 
-
-sim_data_batch <- function(data_seeds, covar_def, outcome_pars,  packages = NULL) {
+sim_data_batch <- function(data_seeds, covar_def, outcome_pars,
+                           mis_scenarios, packages = NULL) {
 
   datlist <- lapply(data_seeds, function(data_seed) {
     future::future(packages = packages, seed = TRUE, {
+
       # simulate complete data
       data_orig <- sim_data(covar_def, outcome_pars, data_seed = data_seed)
 
       # determine grouping structure and levels of each variable
       groups <- simvalidator:::get_groups(
-        setdiff(names(outcome_pars$covar_pars$group_lvls),
-                "lvlone"),
+        setdiff(names(outcome_pars$covar_pars$group_lvls), "lvlone"),
         data_orig)
-      data_lvls <- simvalidator:::cvapply(data_orig,
-                                          simvalidator:::check_varlevel,
-                                          groups = groups)
+
+      data_lvls <- cvapply(data_orig, simvalidator:::check_varlevel,
+                           groups = groups)
 
       compl_data_info <- get_compl_data_info(
         data_orig,
@@ -25,19 +35,29 @@ sim_data_batch <- function(data_seeds, covar_def, outcome_pars,  packages = NULL
         data_lvls = data_lvls
       )
 
+      # create missing values for multiple scenarios
       data_list <- lapply(names(mis_scenarios), function(scen) {
+
+        mis_seed <- simvalidator:::next_seed(data_seed)
+
         data <- structure(
           create_missings(
-            data_orig, mis_scenarios[[scen]],
+            data_orig,
+            prop_mis = mis_scenarios[[scen]]$prop_mis,
+            prop_cc = mis_scenarios[[scen]]$prop_cc,
+            patterns = mis_scenarios[[scen]]$patterns,
+            weights = mis_scenarios[[scen]]$weights,
             idvars = setdiff(names(groups), "lvlone"),
             groups = groups, varlvls = data_lvls,
-            mis_seed = simvalidator:::next_seed(data_seed)),
+            seed = mis_seed)$data,
+
           scenario = scen,
           data_seed = data_seed,
           idvars = names(groups),
           data_lvls = data_lvls
         )
 
+        # get info on incomplete data
         data_info <- simvalidator:::get_miss_data_info(
           data = data,
           data_seed = attr(data, "data_seed"),
@@ -46,8 +66,7 @@ sim_data_batch <- function(data_seeds, covar_def, outcome_pars,  packages = NULL
           data_lvls = attr(data, "data_lvls")
         )
 
-        list(data = data,
-             missing_data_info = data_info)
+        list(data = data, missing_data_info = data_info)
       })
 
       list(data_list = lapply(data_list, "[[", "data"),
